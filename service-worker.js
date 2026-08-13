a7b03f949259875e2e3cee6c1dbc9b01c3fccd5c
Jamal <syedjamalzn@gmail.com>
2026-08-13 07:29:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -108,17 +108,19 @@ self.addEventListener("fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request)
-            .then(response => {
+        caches.match(event.request, {
+            ignoreSearch: true
+        })
+        .then(response => {
 
-                if (response) {
-                    return response;
-                }
+            if (response) {
+                return response;
+            }
 
-                return fetch(event.request);
+            return fetch(event.request);
 
-            })
+        })
 
     );
 
-});
+}); 
